25d4157151c7762daf7730a51531a397c7b30c4b
Pablo Santos <pablo.santos@embedded.ufcg.edu.br>
2016-12-13 16:51:11 -0300

    Alterações no .gitignore

--- index.js
+++ index.js
@@ -4,52 +4,56 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
-var db_string = 'mongodb://127.0.0.1/todolist';
+var db_string = 'mongodb://127.0.0.1/imagelist';
 
 var mongoose = require('mongoose').connect(db_string);
 
 var db = mongoose.connection;
 
-var TodolistItem;
+var Photo;
 
 db.on('error', console.error.bind(console, 'Erro ao conectar ao BD'));
 
 db.once('open', function () {
-    var todolistSchema = mongoose.Schema({
+    var imageSchema = mongoose.Schema({
         title: String,
         created_at: Date
     });
     
-    TodolistItem = mongoose.model('TodolistItem', todolistSchema);
+    Photo = mongoose.model('Photo', imageSchema);
 });
 
-app.listen(5000);
+app.listen(5000, function(){
+	console.log("App on");
+});
 
-app.use(bodyParser.json());
-
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({
-    extended: true
+	limit: '50mb',
+	extended: true
 }));
 
 app.get('/', function (require, response) {
     response.send('Sevidor ON!');
 });
 
-app.get('/todolist', function (req, res) {
-    TodolistItem.find({}, function(error, itens){
+app.get('/imagelist', function (req, res) {
+    Photo.find({}, function(error, itens){
         if(error){
-            res.json('Não foi possivel recuperar a lista de TODO');
+            res.json('Não foi possivel recuperar a lista');
         } else {
             res.json(itens);
         }
     });
 });
 
-app.post('/todolist', function(req, res){
+app.post('/imagelist', function(req, res){
     
     var title = req.param('title');
     
-    new TodolistItem({
+    console.log(title);
+    
+    new Photo({
         'title': title,
         'created_at': new Date()
     }).save(function(error, item){
