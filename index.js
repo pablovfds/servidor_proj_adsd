50b4a085b31e410ef0c826a2d12c2d13e62a8cb5
Pablo Santos <pablo.santos@embedded.ufcg.edu.br>
2017-01-30 09:27:53 -0300

    Atualizada a url de conexão com o banco de dados(local/mlab)

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
-var db_string = 'mongodb://127.0.0.1/imagelist';
+var db_string = 'mongodb://adsd_proj:proj123@ds133438.mlab.com:33438/adsd_proj'; // Heroku
+//var db_string = 'mongodb://127.0.0.1/imagelist'; // Local
 
 var mongoose = require('mongoose').connect(db_string);
 
@@ -16,10 +17,10 @@ db.on('error', console.error.bind(console, 'Erro ao conectar ao BD'));
 
 db.once('open', function () {
     var imageSchema = mongoose.Schema({
-        title: String,
-        created_at: Date
+        'image': String,
+        'created_at': Date
     });
-    
+
     Photo = mongoose.model('Photo', imageSchema);
 });
 
@@ -37,7 +38,7 @@ app.get('/', function (require, response) {
     response.send('Sevidor ON!');
 });
 
-app.get('/imagelist', function (req, res) {
+app.get('/images', function (req, res) {
     Photo.find({}, function(error, itens){
         if(error){
             res.json('Não foi possivel recuperar a lista');
@@ -47,14 +48,11 @@ app.get('/imagelist', function (req, res) {
     });
 });
 
-app.post('/imagelist', function(req, res){
-    
-    var title = req.param('title');
-    
-    console.log(title);
-    
+app.post('/image', function(req, res){
+
+    var image = req.param('image');
     new Photo({
-        'title': title,
+        'image': image,
         'created_at': new Date()
     }).save(function(error, item){
         if(error){
